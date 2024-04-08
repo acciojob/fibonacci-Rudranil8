da164917fa3b721c29f5d60408c22ad8ce5de362
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-08 20:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
+	let mult=1;
+	if(num==0){
+		return 1;
+	}else{
+		for(i=num; i>=1; i--){
+			mult=mult*i;
+		}
+	}
+	return mult;
 }
 
 module.exports = fibonacci;
