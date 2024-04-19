47195621931678998f5d5d024e96aa0eadf50f78
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-19 18:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
+// function fibonacci(num) {
+// 	let a = 0;
+// 	let b = 1;
+// 	for (let i = 2; i <= num; i++) {
+// 	    let sum = a + b;
+// 	    a = b;
+// 	    b = sum;
+// 	}
+// 	if (num==1 || num==2){
+// 		return num-1;
+// 	}else{
+// 		return b;
+// 	}
+// }
+
+// module.exports = fibonacci;
 function fibonacci(num) {
-	let a = 0;
-	let b = 1;
-	for (let i = 2; i <= num; i++) {
-	    let sum = a + b;
-	    a = b;
-	    b = sum;
-	}
-	if (num==1 || num==2){
-		return num-1;
-	}else{
-		return b;
-	}
+    if (num <= 0) {
+        return "Input should be a positive integer";
+    } else if (num == 1) {
+        return 0;
+    }
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i < num; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return a;
 }
 
 module.exports = fibonacci;
